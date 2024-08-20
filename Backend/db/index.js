335e857281e95f06286c0cd00886031d164ad0a9
RaajAakarsh <aakarshraaj032@gmail.com>
2024-08-20 21:36:16 +0530

    Edited environment variables

--- Backend/db/index.js
+++ Backend/db/index.js
@@ -1,9 +1,8 @@
 const mongoose = require("mongoose");
-const DB_URL = import.meta.env.DB_URL;
 
 mongoose
 	.connect(
-		DB_URL
+		process.env.DB_URL
 	)
 	.then(() => console.log("Connected to MongoDB"))
 	.catch((err) => console.error("Error connecting to MongoDB:", err));
