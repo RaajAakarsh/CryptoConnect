b9c662c8c09ddb93a0fa6f1b886d07d4bf7dd8b1
RaajAakarsh <aakarshraaj032@gmail.com>
2024-08-20 18:41:51 +0530

    testing 2

--- Backend/index.js
+++ Backend/index.js
@@ -1,25 +1,43 @@
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const app = express();
+// const userRouter = require("./routes/user");
+// const watchlistRouter = require("./routes/watchlist");
+// const cors = require("cors");
+
+// app.options("*", cors());
+// app.use(cors());
+// app.use(bodyParser.json());
+// app.use("/api/v1/user", userRouter);
+// app.use("/api/v1/watchlist", watchlistRouter);
+
+// // const PORT = 3000;
+// // app
+// // 	.listen(PORT, () => {
+// // 		console.log(`Server is running on port ${PORT}`);
+// // 	})
+// // 	.on("error", (err) => {
+// // 		console.error("Error starting server:", err);
+// // 	});
+
+// module.exports = async (req, res) => {
+// 	return app(req, res);
+// };
+
+
 const express = require("express");
 const bodyParser = require("body-parser");
-const app = express();
+const cors = require("cors");
 const userRouter = require("./routes/user");
 const watchlistRouter = require("./routes/watchlist");
-const cors = require("cors");
 
-app.options("*", cors());
+const app = express();
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/watchlist", watchlistRouter);
 
-// const PORT = 3000;
-// app
-// 	.listen(PORT, () => {
-// 		console.log(`Server is running on port ${PORT}`);
-// 	})
-// 	.on("error", (err) => {
-// 		console.error("Error starting server:", err);
-// 	});
-
 module.exports = async (req, res) => {
-	return app(req, res);
+  return await app(req, res);
 };
